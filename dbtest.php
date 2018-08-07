f348aebc6c4ee758bd0cf3bb83184a662cc075e2
sadjith <sadjith@gmail.com>
2018-08-08 04:14:27 +0530

    data

---
+++ dbtest.php
@@ -0,0 +1,22 @@
+<?php
+
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("username");
+$dbpwd = getenv("password");
+$dbname = getenv("database_name");
+
+$connection = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
+
+$query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
+
+echo " This is the list of users created on database: <br>";
+$rs = $connection->query($query);
+while ($row = mysqli_fetch_assoc($rs)) {
+    echo "User Id: ".$row['user_id'] . " User Name: " . $row['username'] . "<br>";
+}
+echo "End of the list <br>";
+
+mysqli_close($connection);
+
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+Welcome to OpenShift Version3.0.1 -testing database content!!
+
+Try /dbtest.php extension :-)
