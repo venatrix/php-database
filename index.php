47e7962e2703dbdc7a1551e5b4c081e0a0979d53
sadjith <sadjith@venatrix.co.in>
2018-08-08 04:34:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 echo "Welcome to OpenShift Origin v3.9 ";
-echo "<br>To test the database, TRY /dbtest.php URL";
-echo "<br> TRY /dbtest.php URL";
+echo "<br>To test the database ";
+echo "<br> TRY /dbtest.php URL    :)   ";
